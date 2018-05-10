f40a9cd04b85335788553cbbf398174ee2756a04
yuya miyajima <zensupe.xxx@gmail.com>
2018-05-10 17:22:26 +0900

    コミット２

--- ViewController.swift
+++ ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
+        let textLabel = makeTextLabel()
+        self.view.addSubview(textLabel)
+    }
+    
+    func makeTextLabel() -> UILabel {
+        let textLabel = UILabel(frame: CGRectMake(0,0,300,100))
         textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
         textLabel.backgroundColor = UIColor.redColor()
         textLabel.text = "TECH::CAMP"
@@ -24,7 +29,7 @@ class ViewController: UIViewController {
         textLabel.textAlignment = NSTextAlignment.Center
         textLabel.layer.masksToBounds = true
         textLabel.layer.cornerRadius = 20.0
-        self.view.addSubview(textLabel)
+        return textLabel
     }
 
     override func didReceiveMemoryWarning() {
