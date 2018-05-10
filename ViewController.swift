1d553a138f55e875f12487ba2bf8ad742bd2b535
yuya miyajima <zensupe.xxx@gmail.com>
2018-05-10 11:21:28 +0900

    文字変更

--- ViewController.swift
+++ ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
         textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
         textLabel.backgroundColor = UIColor.redColor()
-        textLabel.text = "Hello World!"
+        textLabel.text = "TECH::CAMP"
         textLabel.font = UIFont.systemFontOfSize(40)
         textLabel.textColor = UIColor.whiteColor()
         textLabel.shadowColor = UIColor.blueColor()
